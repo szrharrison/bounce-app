ea98c3c298a475d36835daef5b6d73d26c511dbb
szrharrison <s.z.r.harrison@gmail.com>
2019-01-06 20:59:24 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Bounce App</Text>
+        <Text>This will be the app that Maddy and I make for helping us (her) be ADD</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
